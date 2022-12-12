984b1a763f8c1c83a7454dab32ccf2347be585b2
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-12 14:36:43 -0600

    aria-current

--- components/header.js
+++ components/header.js
@@ -1,28 +1,36 @@
-import React from "react"
-import "./styles/header.scss"
+import React, { useEffect, useRef } from 'react';
+import './styles/header.scss';
 
-import CampSpotsLogo from "images/icons/camp-spots-logo.svg"
-import MegaNav from "./meganav/"
+import CampSpotsLogo from 'images/icons/camp-spots-logo.svg';
+import MegaNav from './meganav/';
 
 const Header = () => {
-    return (
-        <header id="header">
-            <div id="header-nav">
-                <div id="header-logo">
-                    <a href="/" className="header-main-item">
-                        <span className="logo-img">
-                            <img src={CampSpotsLogo} alt="" />
-                        </span>
-                        <span className="logo-text">CampSpots</span>
-                    </a>
-                </div>
-                <MegaNav />
-                <div id="header-login">
-                    <a href="#">Login</a>
-                </div>
-            </div>
-        </header>
-    )
-}
+  const linkRef = useRef(null);
 
-export default Header
+  useEffect(() => {
+    if (document.location.pathname === '/') {
+      linkRef.current.ariaCurrent = 'page';
+    }
+  }, []);
+
+  return (
+    <header id="header">
+      <div id="header-nav">
+        <div id="header-logo">
+          <a href="/" className="header-main-item" ref={linkRef}>
+            <span className="logo-img">
+              <img src={CampSpotsLogo} alt="" />
+            </span>
+            <span className="logo-text">CampSpots</span>
+          </a>
+        </div>
+        <MegaNav />
+        <div id="header-login">
+          <a href="#">Login</a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- components/meganav/subnav-link-group.js
+++ components/meganav/subnav-link-group.js
@@ -1,37 +1,46 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-const SubNavLinkGroup = ({headerText = '', items = []}) => {
-    return (
-        <div className="megamenu-submenu-group">
-            <h3 className="megamenu-submenu-header">
-                {headerText}
-            </h3>
-            <menu>
-            {items.map((item, index) => (
-                <li className="megamenu-submenu-link" key={index}>
-                    <a
-                        className="nav-link"
-                        data-testid={`link-${index}`}
-                        href={item.url}
-                    >
-                        {item.name}
-                    </a>
-                </li>
-            ))}
-            </menu>
-        </div>
-    )
-}
+const SubNavLinkGroup = ({ headerText = '', items = [] }) => {
+  const [currentPathname, setCurrentPathname] = useState(null);
+
+  useEffect(() => {
+    setCurrentPathname(document.location.pathname);
+  }, []);
+
+  const setAriaCurrent = (path) => {
+    return path === currentPathname ? 'page' : null;
+  };
+
+  return (
+    <div className="megamenu-submenu-group">
+      <h3 className="megamenu-submenu-header">{headerText}</h3>
+      <menu>
+        {items.map((item, index) => (
+          <li className="megamenu-submenu-link" key={index}>
+            <a
+              className="nav-link"
+              data-testid={`link-${index}`}
+              href={item.url}
+              aria-current={setAriaCurrent(item.url)}
+            >
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </menu>
+    </div>
+  );
+};
 
 SubNavLinkGroup.propTypes = {
-    headerText: PropTypes.string,
-    items: PropTypes.array
-}
+  headerText: PropTypes.string,
+  items: PropTypes.array,
+};
 
 SubNavLinkGroup.defaultProps = {
-    headerText: '',
-    items: []
-}
+  headerText: '',
+  items: [],
+};
 
-export default SubNavLinkGroup
+export default SubNavLinkGroup;
